343ddf363e0c47d6077670d60e0db9b21425f422
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-03-11 10:05:36 +0100

    mock degit command

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import degit = require('degit');
 import { commands, ExtensionContext, InputBoxOptions, window } from 'vscode';
 
 export function activate(context: ExtensionContext) {
@@ -31,6 +32,29 @@ export function activate(context: ExtensionContext) {
         window.showInformationMessage(`The entered URL was: ${value}`);
       }
     });
+  });
+  context.subscriptions.push(disposable);
+
+  disposable = commands.registerCommand('vsc-degit.degit-mock', () => {
+    const repository = 'WissenIstNacht/widget-caller';
+
+    const emitter = degit(repository);
+    emitter.on('info', (info) => {
+      console.log(info.message);
+    });
+    emitter.on('warn', (info) => {
+      console.warn(info.message);
+    });
+
+    emitter.clone('/Users/vali/Documents/hi/widget-caller').then(
+      () => {
+        window.showInformationMessage(`${repository} successfully degitted.`);
+      },
+      (reason) => {
+        window.showErrorMessage(`error degitting ${repository}.`);
+        console.error('Reason ' + reason);
+      }
+    );
   });
   context.subscriptions.push(disposable);
 }
