b5efc9f6ef8f469e737b9cae811ffd1e5371f912
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-03-20 13:04:19 +0100

    Feature: Prompt user for Destination
    commit 282934e9a3a30e4b02e19bbcd077b60ec5f8fa67
    Author: WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
    Date:   Mon Mar 20 13:00:51 2023 +0100
    
        prompt user for destination

--- src/commands/degitGh.ts
+++ src/commands/degitGh.ts
@@ -1,15 +1,17 @@
 import degit = require('degit');
-import { InputBoxOptions, window } from 'vscode';
+import { InputBoxOptions, OpenDialogOptions, Uri, window } from 'vscode';
 
 import { RegisterableCommand } from '../types';
 
 function degitGhCallback() {
-  userInput().then((inputValue) => {
-    degitHelper(inputValue);
+  inputGhProject().then((inputValue) => {
+    inputDstFolder().then((dst) => {
+      degitHelper(inputValue, dst);
+    });
   });
 }
 
-function degitHelper(repository: string) {
+function degitHelper(repository: string, dst: Uri[]) {
   const emitter = degit(repository);
   emitter.on('info', (info) => {
     console.log(info.message);
@@ -18,7 +20,7 @@ function degitHelper(repository: string) {
     console.warn(info.message);
   });
 
-  emitter.clone(`${LOCAL_DST}/${repository}`).then(
+  emitter.clone(`${dst[0].path}/${repository}`).then(
     () => {
       window.showInformationMessage(`${repository} successfully degitted.`);
     },
@@ -27,24 +29,41 @@ function degitHelper(repository: string) {
       console.error('Reason ' + reason);
     }
   );
-  window.showInformationMessage(`${repository} successfully degitted.`);
 }
 
-async function userInput(): Promise<string> {
+async function inputDstFolder(): Promise<Uri[]> {
+  const options: OpenDialogOptions = {
+    canSelectFolders: true,
+    canSelectFiles: false,
+    title: 'Degit Destination',
+    openLabel: 'Select',
+  };
+  return window.showOpenDialog(options).then((value) => {
+    if (value === undefined) {
+      window.showInformationMessage('The prompt was cancelled');
+      return Promise.reject();
+    } else {
+      return Promise.resolve(value);
+    }
+  });
+}
+
+async function inputGhProject(): Promise<string> {
   const options: InputBoxOptions = {
     ignoreFocusOut: true,
     placeHolder: 'Github Repository',
     prompt: 'Enter a Github repository to degit',
-    validateInput: (value: string) => {
-      function isErroneousInput(value: string) {
-        const regExpResult = value.match(GH_PROJECT_VALIDATION_REGEXP);
-        return regExpResult
-          ? undefined
-          : 'String must be a of tye <Username>/<Projectname>';
-      }
+    // FIXME Validation doesn't react properly
+    // validateInput: (inputBoxValue: string) => {
+    //   function isErroneousInput(value: string) {
+    //     const regExpResult = value.match(GH_PROJECT_VALIDATION_REGEXP);
+    //     return regExpResult
+    //       ? undefined
+    //       : 'String must be a of tye <Username>/<Projectname>';
+    //   }
 
-      return isErroneousInput(value);
-    },
+    //   return isErroneousInput(inputBoxValue);
+    // },
   };
 
   return window.showInputBox(options).then((value) => {
