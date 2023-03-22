bc3f19c6f55c265b41938b1055536cda471cbc36
WissenIstNacht <32465601+WissenIstNacht@users.noreply.github.com>
2023-03-22 09:50:27 +0100

    Chore: Delete Mock Commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,4 @@
 import { RegisterableCommand } from '../types';
-import { basicDegitCommand } from './basicDegit';
 import { degitGhCommand } from './degitGh';
-import { degitHelloWorldCommand } from './degitHelloWorld';
-import { urlInputCommand } from './urlInput';
 
-export const registerableCommands: RegisterableCommand[] = [
-  basicDegitCommand,
-  urlInputCommand,
-  degitHelloWorldCommand,
-  degitGhCommand,
-];
+export const registerableCommands: RegisterableCommand[] = [degitGhCommand];
